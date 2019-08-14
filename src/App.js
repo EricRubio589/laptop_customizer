885de5a7913f49c047b9d85a5c84923ae1f89f64
Eric Rubio <eric.rubio.services@gmail.com>
2019-08-14 15:09:48 -0500

    started separating components

--- src/App.js
+++ src/App.js
@@ -75,50 +75,57 @@ class App extends Component {
       );
     });
 
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
 
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
+    //////////////////////////Moved to Cart.js//////////////////////////////////
+    // const summary = Object.keys(this.state.selected).map((feature, idx) => {
+    //   const featureHash = feature + '-' + idx;
+    //   const selectedOption = this.state.selected[feature];
 
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
+    //   return (
+    //     <div className="summary__option" key={featureHash}>
+    //       <div className="summary__option__label">{feature} </div>
+    //       <div className="summary__option__value">{selectedOption.name}</div>
+    //       <div className="summary__option__cost">
+    //         {USCurrencyFormat.format(selectedOption.cost)}
+    //       </div>
+    //     </div>
+    //   );
+    // });
+    //////////////////////////////////////////////////////////////////////////////
 
-    return (
-      <div className="App">
-        <header>
-          <h1>ELF Computing | Laptops</h1>
-        </header>
-        <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {features}
-          </form>
-          <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
-          </section>
-        </main>
-      </div>
-    );
-  }
+
+    ///////////////////// Moved to Total.js /////////////////////////////
+    // const total = Object.keys(this.state.selected).reduce(
+    //   (acc, curr) => acc + this.state.selected[curr].cost,
+    //   0
+    // );
+    //
+    // return (
+    //   <div className="App">
+    //     <header>
+    //       <h1>ELF Computing | Laptops</h1>
+    //     </header>
+    //     <main>
+    //       <form className="main__form">
+    //         <h2>Customize your laptop</h2>
+    //         {features}
+    //       </form>
+    //       <section className="main__summary">
+    //         <h2>Your cart</h2>
+    //         {summary}
+    //         <div className="summary__total">
+    //           <div className="summary__total__label">Total</div>
+    //           <div className="summary__total__value">
+    //             {USCurrencyFormat.format(total)}
+    //           </div>
+    //         </div>
+    //       </section>
+    //     </main>
+    //   </div>
+    // );
+    //////////////////////////////////////////////////////////////////////
+
+  } 
 }
 
 export default App;
